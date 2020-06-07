bad110c790b47fa58682f7701ab3ceec163d52d4
linnefromice <linnefromice@gmail.com>
2020-06-08 02:31:13 +0900

    update fifth - update logic (add toast when touched item)

--- todo_list_kotlin_one/app/src/main/java/com/example/todo_list_kotlin_one/MainActivity.kt
+++ todo_list_kotlin_one/app/src/main/java/com/example/todo_list_kotlin_one/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.todo_list_kotlin_one
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.SparseBooleanArray
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -29,7 +31,16 @@ class MainActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
         }
 
-        //Adding the toast message to the list when an item on the list is pressed
+        // Adding the toast message to the list when an item on the list is pressed
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            android.widget.Toast.makeText(
+                this,
+                "You selected the item --> ${itemList.get(i)}",
+                android.widget.Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        // Selecting and Deleting the items from the list when the delete button is pressed
         delete.setOnClickListener {
             val position: SparseBooleanArray = listView.checkedItemPositions
             val count = listView.count
