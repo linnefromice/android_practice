0736ceef1faf02b3d5c2d5c41d6463f157896fdc
linnefromice <linnefromice@gmail.com>
2020-06-07 23:12:38 +0900

    update fourth - finished step 6 (add dao interface)

---
+++ room_word_sample/app/src/main/java/com/example/room_word_sample/WordDao.kt
@@ -0,0 +1,18 @@
+package com.example.room_word_sample
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
