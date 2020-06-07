6d7162912027613e6558762c192b46492a853a97
linnefromice <linnefromice@gmail.com>
2020-06-08 02:27:13 +0900

    update fifth - create logic

--- todo_list_kotlin_one/app/src/main/java/com/example/todo_list_kotlin_one/MainActivity.kt
+++ todo_list_kotlin_one/app/src/main/java/com/example/todo_list_kotlin_one/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.todo_list_kotlin_one
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initializing the array lists and the adapter
+        var itemList = arrayListOf<String>()
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemList)
+
+        // Adding the items to the list when the add button is pressed
+        add.setOnClickListener {
+            itemList.add(editText.text.toString())
+            listView.adapter = adapter
+            adapter.notifyDataSetChanged()
+            editText.text.clear()
+        }
+
+        // Clearing all the items in the list when the clear button is pressed
+        clear.setOnClickListener {
+            itemList.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        //Adding the toast message to the list when an item on the list is pressed
+        delete.setOnClickListener {
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item = count - 1
+            while (item >= 0) {
+                if (position.get(item)) {
+                    adapter.remove(itemList.get(item))
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+
     }
 }
