1aeb9e80b7c96cf7c3de964476f25eb2a36c6677
linnefromice <linnefromice@gmail.com>
2020-06-08 22:35:08 +0900

    update sixth - modify package constructure & create layout

--- kotlin_app_two/app/src/main/java/com/example/kotlin_app_two/ui/quotes/QuotesActivity.kt
+++ kotlin_app_two/app/src/main/java/com/example/kotlin_app_two/ui/quotes/QuotesActivity.kt
@@ -1,9 +1,10 @@
-package com.example.kotlin_app_two
+package com.example.kotlin_app_two.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_app_two.R
 
-class MainActivity : AppCompatActivity() {
+class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
