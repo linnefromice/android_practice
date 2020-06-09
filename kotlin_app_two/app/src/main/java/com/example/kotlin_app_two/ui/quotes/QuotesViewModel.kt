36b53f94231098078cf7cfee749ac201ec1b2433
linnefromice <linnefromice@gmail.com>
2020-06-09 22:38:52 +0900

    update sixth - update viewmodel (Original)

--- kotlin_app_two/app/src/main/java/com/example/kotlin_app_two/ui/quotes/QuotesViewModel.kt
+++ kotlin_app_two/app/src/main/java/com/example/kotlin_app_two/ui/quotes/QuotesViewModel.kt
@@ -1,11 +1,29 @@
 package com.example.kotlin_app_two.ui.quotes
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kotlin_app_two.data.FakeDatabase
 import com.example.kotlin_app_two.data.Quote
 import com.example.kotlin_app_two.data.QuoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 // QuoteRepository dependency will again be passed in the constructor using dependency injection
-class QuotesViewModel(private val quoteRepository: QuoteRepository) : ViewModel() {
-    fun getQuotes() = quoteRepository.getQuotes()
-    fun addQuote(quote: Quote) = quoteRepository.addQuote(quote)
+class QuotesViewModel : ViewModel() {
+    private val quoteRepository: QuoteRepository = QuoteRepository.getInstance(FakeDatabase.getInstance().quoteDao)
+    private val quotes: LiveData<List<Quote>>
+
+    init {
+        // TEST DATA
+        addQuote(Quote(quoteText = "How are you?", author = "Author One"))
+        addQuote(Quote(quoteText = "I'm sorry...", author = "Author Two"))
+        addQuote(Quote(quoteText = "Thank you!", author = "Author Three"))
+        quotes = quoteRepository.getQuotes()
+    }
+
+    fun getQuotes() = quotes
+    fun addQuote(quote: Quote) = viewModelScope.launch(Dispatchers.IO) {
+        quoteRepository.addQuote(quote)
+    }
 }
