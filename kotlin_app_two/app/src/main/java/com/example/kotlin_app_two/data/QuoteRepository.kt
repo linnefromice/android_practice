9ec5fb9dcea7b922c17ab7cb68487ff15789b036
linnefromice <linnefromice@gmail.com>
2020-06-09 21:54:18 +0900

    update sixth - create repository

---
+++ kotlin_app_two/app/src/main/java/com/example/kotlin_app_two/data/QuoteRepository.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin_app_two.data
+
+class QuoteRepository private constructor(private val quoteDao: FakeQuoteDao) {
+
+    fun addQuote(quote: Quote) {
+        quoteDao.addQuote(quote)
+    }
+
+    fun getQuotes() = quoteDao.getQuotes()
+
+    companion object {
+        @Volatile private var instance: QuoteRepository? = null
+
+        fun getInstance(quoteDao: FakeQuoteDao) =
+            instance ?: synchronized(this) {
+                instance ?: QuoteRepository(quoteDao).also { instance = it }
+            }
+    }
+}
