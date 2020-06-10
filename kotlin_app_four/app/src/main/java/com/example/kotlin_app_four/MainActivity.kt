9775cbc4306cba27000a07eddde23858d237795a
linnefromice <linnefromice@gmail.com>
2020-06-11 01:22:29 +0900

    update ninth - update activity & modify component id

--- kotlin_app_four/app/src/main/java/com/example/kotlin_app_four/MainActivity.kt
+++ kotlin_app_four/app/src/main/java/com/example/kotlin_app_four/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.kotlin_app_four
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 比較用のアクティビティに属するカウンター
+        var counterA = 0
+        // インスタンスを作成
+        val viewModel: CountViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java)
+
+        countButton.setOnClickListener {
+            counterA++
+            viewModel.counterB++
+
+            textOne.setText(counterA.toString())
+            textTwo.setText(viewModel.counterB.toString())
+        }
     }
 }
