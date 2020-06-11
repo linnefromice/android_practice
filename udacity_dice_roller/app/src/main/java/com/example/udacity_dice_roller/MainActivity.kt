b17d4239cfd0c5b0cd3296c52f861f27a967d49d
linnefromice <linnefromice@gmail.com>
2020-06-12 01:48:53 +0900

    update udacity curriculum project - finished 1-18

--- udacity_dice_roller/app/src/main/java/com/example/udacity_dice_roller/MainActivity.kt
+++ udacity_dice_roller/app/src/main/java/com/example/udacity_dice_roller/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.udacity_dice_roller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        roll_button.text = "Let's Roll"
+        roll_button.setOnClickListener {
+            // 1-17
+            // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rolLDice()
+        }
+    }
+
+    private fun rolLDice() {
+        val randomInt: Int = Random().nextInt(6) + 1
+        result_text.text = randomInt.toString()
     }
 }
