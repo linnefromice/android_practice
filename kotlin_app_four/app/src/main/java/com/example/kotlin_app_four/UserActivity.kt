ccb6f7fd30380c23c048729cd6e64b4cad694ade
linnefromice <linnefromice@gmail.com>
2020-06-11 13:22:06 +0900

    update ninth - use DataBinding to user activity

--- kotlin_app_four/app/src/main/java/com/example/kotlin_app_four/UserActivity.kt
+++ kotlin_app_four/app/src/main/java/com/example/kotlin_app_four/UserActivity.kt
@@ -2,10 +2,22 @@ package com.example.kotlin_app_four
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.example.kotlin_app_four.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        // ViewModelのインスタンスを作成
+        val viewModel: UserViewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
+        // DataBindingのインスタンスを作成(onCreateの外でも良い)
+        val binding: ActivityUserBinding = DataBindingUtil.setContentView(this, R.layout.activity_user)
+        // ViewModelのインスタンスを設定
+        binding.user = viewModel
+        // ライフサイクル所有者を設定
+        binding.lifecycleOwner = this
     }
 }
