428259999be4f2f977099bc2bea992977ce02573
linnefromice <linnefromice@gmail.com>
2020-06-13 18:37:38 +0900

    update udacity curriculum project - finished 2-17

--- udacity_about_me/app/src/main/java/com/example/udacity_about_me/MainActivity.kt
+++ udacity_about_me/app/src/main/java/com/example/udacity_about_me/MainActivity.kt
@@ -5,24 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import com.example.udacity_about_me.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        done_button.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val myName = MyName(name = "Aleks Haecky", nickname = "Alex")
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(it: View) {
-        nickname_text.text = nickname_edit.text
-        nickname_edit.visibility = View.GONE
-        done_button.visibility = View.GONE
-        nickname_text.visibility = View.VISIBLE
-
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(it.windowToken, 0)
     }
---
+++ udacity_about_me/app/src/main/java/com/example/udacity_about_me/MyName.kt
@@ -0,0 +1,6 @@
+package com.example.udacity_about_me
+
+data class MyName(
+    var name: String = "",
+    var nickname: String = ""
+)
