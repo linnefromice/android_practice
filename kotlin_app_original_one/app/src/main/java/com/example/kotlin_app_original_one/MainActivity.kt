fe9ddcebd0c6a3c77657a6ff97d0cd8f3356932d
linnefromice <linnefromice@gmail.com>
2020-06-15 00:15:13 +0900

    update original project - add button action (judge answer)

--- kotlin_app_original_one/app/src/main/java/com/example/kotlin_app_original_one/MainActivity.kt
+++ kotlin_app_original_one/app/src/main/java/com/example/kotlin_app_original_one/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.kotlin_app_original_one
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.kotlin_app_original_one.databinding.ActivityMainBinding
 
@@ -16,8 +19,25 @@ class MainActivity : AppCompatActivity() {
             title = "Game",
             description = "What is the game title from Square Enix?",
             proposedAnswerList = arrayOf<String>("BioHazard", "StarOcean", "FinalFantasy", "SuperMario"),
-            answerIndex = 3
+            answerIndex = 2
         )
         binding.quiz = quiz
+        addActionToButtons(quiz)
+    }
+
+    private fun addActionToButtons(quiz: Quiz) {
+        val correctAnswer: String = quiz.proposedAnswerList[quiz.answerIndex]
+        binding.buttonOne.setOnClickListener { createResultToast(quiz.proposedAnswerList[0], correctAnswer)}
+        binding.buttonTwo.setOnClickListener { createResultToast(quiz.proposedAnswerList[1], correctAnswer)}
+        binding.buttonThree.setOnClickListener { createResultToast(quiz.proposedAnswerList[2], correctAnswer)}
+        binding.buttonFour.setOnClickListener { createResultToast(quiz.proposedAnswerList[3], correctAnswer)}
+    }
+
+    private fun createResultToast(answer: String, correctAnswer: String) {
+        if (answer == correctAnswer) {
+            Toast.makeText(applicationContext, "Correct!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Incorrect...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
