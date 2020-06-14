118d7b0a4933f4cbc4e318e9c65e698c2bb78293
linnefromice <linnefromice@gmail.com>
2020-06-14 23:56:30 +0900

    update original project - update layout using binding data

--- kotlin_app_original_one/app/src/main/java/com/example/kotlin_app_original_one/MainActivity.kt
+++ kotlin_app_original_one/app/src/main/java/com/example/kotlin_app_original_one/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.kotlin_app_original_one
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.kotlin_app_original_one.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val quiz: Quiz = Quiz(
+            id = 1,
+            title = "Game",
+            description = "What is the game title from Square Enix?",
+            proposedAnswerList = arrayOf<String>("BioHazard", "StarOcean", "FinalFantasy", "SuperMario"),
+            answerIndex = 3
+        )
+        binding.quiz = quiz
     }
 }
