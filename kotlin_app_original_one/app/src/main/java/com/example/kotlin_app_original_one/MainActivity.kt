9e8aa976c5de40fc9f66e5eaae04ca675612220c
linnefromice <linnefromice@gmail.com>
2020-06-25 01:15:23 +0900

    update original app - add quiz pattern & select quiz in random

--- kotlin_app_original_one/app/src/main/java/com/example/kotlin_app_original_one/MainActivity.kt
+++ kotlin_app_original_one/app/src/main/java/com/example/kotlin_app_original_one/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin_app_original_one
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,20 +8,53 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.kotlin_app_original_one.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val quiz: Quiz = Quiz(
+    private var quizList: MutableList<Quiz> = mutableListOf<Quiz>(
+        Quiz(
             id = 1,
             title = "Game",
             description = "What is the game title from Square Enix?",
             proposedAnswerList = arrayOf<String>("BioHazard", "StarOcean", "FinalFantasy", "SuperMario"),
             answerIndex = 2
+        ),
+        Quiz(
+            id = 2,
+            title = "Game",
+            description = "What is the hero from Nintendo?",
+            proposedAnswerList = arrayOf<String>("Kuribo", "Wario", "Peach", "Mario"),
+            answerIndex = 3
+        ),
+        Quiz(
+            id = 3,
+            title = "Game",
+            description = "Which word fill this title, XXX brothers, meaning popular game from Nintendo.",
+            proposedAnswerList = arrayOf<String>("Smash", "Attack", "Slush", "Crash"),
+            answerIndex = 0
+        ),
+        Quiz(
+            id = 4,
+            title = "Game",
+            description = "What is the game hard name from Sony?",
+            proposedAnswerList = arrayOf<String>("Play Station", "Dream Cast", "Nintendo 64", "Sega Saturn"),
+            answerIndex = 0
+        ),
+        Quiz(
+            id = 5,
+            title = "Game",
+            description = "Which weapon is main equipment pf Link?",
+            proposedAnswerList = arrayOf<String>("Gun", "Bomb", "Sword", "Spear"),
+            answerIndex = 2
         )
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val randomInt = Random.nextInt(quizList.size)
+        val quiz: Quiz = quizList[randomInt]
         binding.quiz = quiz
         addActionToButtons(quiz)
     }
@@ -36,6 +70,8 @@ class MainActivity : AppCompatActivity() {
     private fun createResultToast(answer: String, correctAnswer: String) {
         if (answer == correctAnswer) {
             Toast.makeText(applicationContext, "Correct!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, MainActivity::class.java)
+            startActivity(intent)
         } else {
             Toast.makeText(applicationContext, "Incorrect...", Toast.LENGTH_SHORT).show()
         }
